07f9aa4bae0f942dbabf12027e16f917d7c71a62
sharadwaghmare <sharadwaghmare93@gmail.com>
2022-08-15 21:17:59 +0530

    solve

--- Day-1/one.js
+++ Day-1/one.js
@@ -1,1 +1,53 @@
-console.log("hello word")
+// // conver array to string 
+// if numer is less than 0 add 0 to string
+
+ const input1=[2,-4,6,8,-9]
+const input2=[1,2,3,4]
+// str = " ";
+// // for(i =0; i<input1.length; i++){
+// //     if(input1[i]>0){
+// //         str = str + input1[i]
+// //     }
+// //     else{
+// //         str = str + 0
+// //     }
+// // }
+// // console.log(str)
+
+// for(i=0; i<input2.length; i++){
+//     if(input2[i]>0){
+//         str = str + input2[i]
+//     }
+//     else{
+//         str = str + input2[i]
+//     }
+// }
+// console.log(str)
+
+
+
+
+
+
+function arraytostriNg(arr){
+    var str=""
+for(i=0; i<arr.length; i++){
+    if(arr[i]>0){
+        str = str + arr[i]
+    }
+    else{
+        str = str + 0
+    }
+}
+console.log(str)
+}
+
+arraytostriNg([2,-2,3,5])
+
+
+
+
+
+
+
+
