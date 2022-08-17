7538f883eb6987d1418b4e3794ac41645a1990f3
sharadwaghmare <sharadwaghmare93@gmail.com>
2022-08-17 20:58:38 +0530

    ques

---
+++ Day-3/home.js
@@ -0,0 +1,10 @@
+//  Javascript Program to Check if a Number is Odd or Even
+const num = 2;
+if(num % 2 == 0){
+    console.log("The num is even")
+}
+else{
+    console.log("This num is odd")
+}
+
+
