628877ea0bc47f27292c6511485d53df5a098913
sharadwaghmare <sharadwaghmare93@gmail.com>
2022-08-18 21:12:38 +0530

    prolem solved

--- Day-1/one.js
+++ Day-1/one.js
@@ -3,26 +3,26 @@
 
  const input1=[2,-4,6,8,-9]
 const input2=[1,2,3,4]
-// str = " ";
-// // for(i =0; i<input1.length; i++){
-// //     if(input1[i]>0){
-// //         str = str + input1[i]
-// //     }
-// //     else{
-// //         str = str + 0
-// //     }
-// // }
-// // console.log(str)
-
-// for(i=0; i<input2.length; i++){
-//     if(input2[i]>0){
-//         str = str + input2[i]
+str = " ";
+// for(i =0; i<input1.length; i++){
+//     if(input1[i]>0){
+//         str = str + input1[i]
 //     }
 //     else{
-//         str = str + input2[i]
+//         str = str + 0
 //     }
 // }
 // console.log(str)
+
+for(i=0; i<input2.length; i++){
+    if(input2[i]>0){
+        str = str + input2[i]
+    }
+    else{
+        str = str + input2[i]
+    }
+}
+console.log(str)
 
 
 
