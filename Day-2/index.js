c5ff9ea936519816d6088579f5198c9e604b0c9f
sharadwaghmare <sharadwaghmare93@gmail.com>
2022-08-18 19:57:11 +0530

    while prolem

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,4 +1,21 @@
 const arr1=[2,-4,6,8,-9]
 const arr2=[1,2,3,4]
 
+//while loop
 
+function str(arr){
+    var i=0
+    var ans=""
+    while(i<arr.length){
+        if(arr[i]<0){
+            ans=ans+0
+        }
+        else{
+            ans=ans+arr[i]
+        }
+        i++
+    }
+    console.log(ans)
+}
+
+str(arr1)
