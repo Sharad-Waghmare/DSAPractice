1117081498beed5e9d3184df98ef80de3f6383b5
sharadwaghmare <sharadwaghmare93@gmail.com>
2022-09-01 18:57:48 +0530

    Divide & conquerer Technique

---
+++ Day-14/fourteen.js
@@ -0,0 +1,17 @@
+function searchAlogNum(array, num){
+    let min = 0;
+    let max = array.length-1;
+    while(min <= max){
+        let midIndex = Math.floor((min + max) / 2);
+        if(array[midIndex] < num){
+            min = midIndex + 1;
+        }else if(array[midIndex] > num){
+            max = midIndex -1;
+        }else{
+            return midIndex;
+        }
+    }
+    return -1
+}
+const result = searchAlogNum([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],7);
+console.log(result);
